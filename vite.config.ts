bf09b11158b2cec7865dbb2f1a50f4b1576a1407
Harry Daniel Mendez <harrymndz3@gmail.com>
2026-06-01 19:14:21 -0600

    fix: separate vitest config and fix type import

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    // sin jsdom porque los tests son funciones puras
-    environment: 'node',
-    setupFiles: [],
-  },
 })
